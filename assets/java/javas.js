ebc0a9bc42e7a9125c6276bb2df9a7e961f61310
Nicholas Gallardo <nic.gallardo@gmail.com>
2015-07-22 23:23:37 -0600

    assets

---
+++ assets/java/javas.js
@@ -0,0 +1,27 @@
+
+
+$(document).ready(function() {
+  $('.showtrigger').click(function(){
+    $(this).next(".showtarget").toggleClass("active");
+  });
+
+
+    $(window).scroll( function(){
+
+        $('.hideme').each( function(i){
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},1000);
+
+            }
+
+        });
+
+    });
+
+});
